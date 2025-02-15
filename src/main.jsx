87490beebd5b5722c9b626a1c734ef359e2c6881
Winsome Tang <tangwins@sheridancollege.ca>
2025-02-15 16:25:28 -0500

    Fix redeployment error

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import ConstructionPage from './construction/ConstructionPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <ConstructionPage />
   </StrictMode>,
 )
